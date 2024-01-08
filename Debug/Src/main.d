0106dc52309590737cb0ff373554664b5ede9e03
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2024-01-08 23:06:13 +0800

    Create MCU specific headerfile and include path settings

---
+++ Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/lenovo/Documents/batcave/STM32CubeIDE/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+C:/Users/lenovo/Documents/batcave/STM32CubeIDE/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
---
+++ drivers/Inc/stm32f407xx.h
@@ -0,0 +1,13 @@
+/*
+ * stm32f407xx.h
+ *
+ *  Created on: Jan 8, 2024
+ *      Author: Kyl Justin Perez
+ */
+
+#ifndef INC_STM32F407XX_H_
+#define INC_STM32F407XX_H_
+
+
+
+#endif /* INC_STM32F407XX_H_ */
