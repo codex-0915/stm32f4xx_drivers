cbc1de96fb7464a67999c03f5cec49dca95e50ba
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2024-01-09 23:27:38 +0800

    Add base address C macros of MCU's embedded memories

--- drivers/Inc/stm32f407xx.h
+++ drivers/Inc/stm32f407xx.h
@@ -8,6 +8,11 @@
 #ifndef INC_STM32F407XX_H_
 #define INC_STM32F407XX_H_
 
-
+/* Define the base addresses of the Flash and SRAM memories */
+#define FLASH_BASEADDR (0x08000000U)
+#define SRAM1_BASEADDR (0x20000000U)
+#define SRAM2_BASEADDR (0x2001C000U)
+#define ROM (0x1FFF0000U) // Also known as system memory
+#define SRAM (SRAM1_BASEADDR)
 
 #endif /* INC_STM32F407XX_H_ */
