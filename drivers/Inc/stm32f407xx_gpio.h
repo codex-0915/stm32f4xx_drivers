aa7ed58b3fe443b614c6cda0f2d24c2780d517e5
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2024-09-09 20:38:55 +0800

    Update existing and add new gpio macros

--- drivers/Inc/stm32f407xx_gpio.h
+++ drivers/Inc/stm32f407xx_gpio.h
@@ -24,9 +24,9 @@
 /* Configuration structure for a GPIO pin */
 typedef struct
 {
-    uint8_t GPIO_PinNumber;
-    uint8_t GPIO_PinMode;
-    uint8_t GPIO_PinSpeed;
+    uint8_t GPIO_PinNumber;         /* Possible values from "GPIO pin numbers" (0-15) */
+    uint8_t GPIO_PinMode;           /* Possible values from "GPIO pin modes" (0-6) */
+    uint8_t GPIO_PinSpeed;          /* Possible values from "GPIO pin possible output speed" (0-3) */
     uint8_t GPIO_PinPuPdControl;
     uint8_t GPIO_PinOPType;
     uint8_t GPIO_PinAltFunMode;
@@ -39,8 +39,25 @@ typedef struct
     GPIO_PinConfig_t GPIO_PinCfg;   /* GPIO pin configuration settings */
 } GPIO_Handle_t;
 
+/* GPIO pin numbers */
+#define GPIO_PIN_NO_0           (0)
+#define GPIO_PIN_NO_1           (1)
+#define GPIO_PIN_NO_2           (2)
+#define GPIO_PIN_NO_3           (3)
+#define GPIO_PIN_NO_4           (4)
+#define GPIO_PIN_NO_5           (5)
+#define GPIO_PIN_NO_6           (6)
+#define GPIO_PIN_NO_7           (7)
+#define GPIO_PIN_NO_8           (8)
+#define GPIO_PIN_NO_9           (9)
+#define GPIO_PIN_NO_10          (10)
+#define GPIO_PIN_NO_11          (11)
+#define GPIO_PIN_NO_12          (12)
+#define GPIO_PIN_NO_13          (13)
+#define GPIO_PIN_NO_14          (14)
+#define GPIO_PIN_NO_15          (15)
 
-/* GPIO modes */
+/* GPIO pin modes */
 #define GPIO_MODE_INPUT         (0)  /* GPIO Input Mode */
 #define GPIO_MODE_OUTPUT        (1)  /* GPIO Output Mode */
 #define GPIO_MODE_ALTFUNC       (2)  /* GPIO Alternate Function Mode */
@@ -49,6 +66,20 @@ typedef struct
 #define GPIO_MODE_IT_RT         (5)  /* Input Mode Rising Edge */
 #define GPIO_MODE_IT_FTRT       (6)  /* Input Mode Falling-Rising Edge */
 
+/* GPIO pin possible output types */
+#define GPIO_OP_TYPE_PP         (0)  /* GPIO Output Push Pull */
+#define GPIO_OP_TYPE_OD         (1)  /* GPIO Output Open Drain */
+
+/* GPIO pin possible output speed */
+#define GPIO_SPEED_LOW          (0)  /* GPIO Output Speed Low */
+#define GPIO_SPEED_MEDIUM       (1)  /* GPIO Output Speed Medium */
+#define GPIO_SPEED_FAST         (2)  /* GPIO Output Speed Fast */
+#define GPIO_SPEED_HIGH         (3)  /* GPIO Output Speed High */
+
+/* GPIO pin pull up and pull down configuration macros */
+#define GPIO_NO_PUPD            (0)
+#define GPIO_PIN_PU             (1)
+#define GPIO_PIN_PD             (2)
 
 /***************************************************************************************************/
 /********************************************** Prototypes *****************************************/
